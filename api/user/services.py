0bff6ec7181defb8b8904ae67273748eb1223d0e
petruha <refloader@gmail.com>
2023-04-08 04:54:52 +0300

    Impl del user by id

--- api/user/services.py
+++ api/user/services.py
@@ -1,11 +1,12 @@
 from typing import Optional
 
 from fastapi import HTTPException, status
+from sqlalchemy.orm import Session
 
 from . import models
 
 
-async def new_user_register(request, database) -> models.User:
+async def new_user_register(request, database: Session) -> models.User:
     new_user = models.User(
         username=request.username,
         email=request.email,
@@ -19,8 +20,13 @@ async def new_user_register(request, database) -> models.User:
     return new_user
 
 
-async def get_user_by_id(user_id, database) -> Optional[models.User]:
+async def get_user_by_id(user_id, database: Session) -> Optional[models.User]:
     user_info = database.query(models.User).get(user_id)
     if not user_info:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Data Not Found!")
     return user_info
+
+
+async def delete_user_by_id(user_id, database: Session):
+    database.query(models.User).filter(models.User.id == user_id).delete()
+    database.commit()
